211a0a3a2d766bcaeea8d065ef2db99e1df3a057
lukblan <luk.blanko@gmail.com>
2024-05-31 20:52:57 -0300

    Add vite checker

--- config/vite.config.js
+++ config/vite.config.js
@@ -1,5 +1,7 @@
 import { defineConfig } from 'vite'
+import pluginChecker from 'vite-plugin-checker';
 
 export default defineConfig({
   base: './',
+  plugins: [pluginChecker({ typescript: {root: "./config"} })]
 })
