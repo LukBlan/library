305716515f4033459cb7128bc73d92904280bafd
lukblan <luk.blanko@gmail.com>
2024-06-10 23:48:28 -0300

    Add tailwind palette

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -3,7 +3,18 @@ export default {
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
+  darkMode: 'selector',
   theme: {
+
+    colors: {
+      "white": "#ffffff",
+      "grey": "#56565f",
+      "grey-light": "#f1f3f8",
+      "violet": "#7d77d5",
+      "violet-light": "#e6ecfa",
+      "night": "#131116",
+      "night-3": "#0D0C0F",
+    },
     extend: {},
   },
   plugins: [],
